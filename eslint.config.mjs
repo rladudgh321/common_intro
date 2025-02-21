392761cc63b7d63ba6bab7dfd62e77cec547e86a
ungok <kyh_0625@naver.com>
2025-02-21 15:14:34 +0900

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier: fixupPluginRules(prettier),
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      'no-restricted-globals': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div>
+            {children}
+        </div>
       </body>
     </html>
   );
